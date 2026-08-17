0b6b9ae1272b84cd93bba4744c2cc7076b9e06cf
miyagi2026-coder <miyagi19650210@gmail.com>
2026-08-17 22:21:27 +0900

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,9 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-
 export default function Document() {
   return (
     <Html>
       <Head>
+        <meta name="google-site-verification" content="NiT5DfOcEDXqi7czhb65gyT61vDJ4CLmVpVDsvTYtjM" />
         <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7572849961321548"
