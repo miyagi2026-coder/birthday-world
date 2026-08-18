862a032b13ce36b24bc64ace38725c9132db851a
miyagi2026-coder <miyagi19650210@gmail.com>
2026-08-18 11:20:58 +0900

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,29 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+
 export default function Document() {
   return (
     <Html>
       <Head>
+        {/* Google Search Console */}
         <meta name="google-site-verification" content="NiT5DfOcEDXqi7czhb65gyT61vDJ4CLmVpVDsvTYtjM" />
+
+        {/* Google Analytics */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-Z4KL5CCJVR"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-Z4KL5CCJVR');
+            `,
+          }}
+        />
+
+        {/* Google AdSense */}
         <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7572849961321548"
